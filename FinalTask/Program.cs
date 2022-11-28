b8f4ded3fa0c7702e72d345c20707f0700c27b3d
medvedit <medwed.sa.job@gmail.com>
2022-11-28 21:44:27 +0300

    add array

--- FinalTask/Program.cs
+++ FinalTask/Program.cs
@@ -1,2 +1,2 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] array = new string[6] {"473", "-2", "hello", "world", "757t", ":-)"};
+string[] result = new string[array.Length];
