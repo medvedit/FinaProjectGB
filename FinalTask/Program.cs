f3c87b904c1e5016f87df0698c981fb7d3aee844
medvedit <medwed.sa.job@gmail.com>
2022-11-28 21:49:36 +0300

    add function lengthOfArrayElements

--- FinalTask/Program.cs
+++ FinalTask/Program.cs
@@ -1,2 +1,23 @@
 ﻿string[] array = new string[6] {"473", "-2", "hello", "world", "757t", ":-)"};
 string[] result = new string[array.Length];
+
+
+
+
+
+
+
+
+
+void lengthOfArrayElements(string[] arr1, string[] arr2)
+{
+    int count = 0;
+    for (int i = 0; i < arr1.Length; i++)
+    {
+        if(arr1[count].Length <= 3)
+        {
+            arr2[count] = arr1[i];
+            count++;
+        }
+    }
+}
